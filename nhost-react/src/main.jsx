ddb514e5a99708074d9a0eb48207391b1d47f263
itz-maeee <127337722+itz-maeee@users.noreply.github.com>
2025-08-15 10:46:18 +0530

    Fix Authenticated component import error

--- nhost-react/src/main.jsx
+++ nhost-react/src/main.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ApolloProvider } from '@apollo/client'
-import { NhostProvider, Authenticated } from '@nhost/react'
+import { NhostProvider } from '@nhost/react'
 import { nhost } from './lib/nhost'
 import { apolloClient } from './lib/apollo'
 import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<NhostProvider nhost={nhost}>
-{/* Only render the app when the user is authenticated */}
-<Authenticated fallback={<div>Please sign in…</div>}>
-<ApolloProvider client={apolloClient}>
-<App />
-</ApolloProvider>
-</Authenticated>
-</NhostProvider>
-</React.StrictMode>
+  <React.StrictMode>
+    <NhostProvider nhost={nhost}>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </NhostProvider>
+  </React.StrictMode>
 )
